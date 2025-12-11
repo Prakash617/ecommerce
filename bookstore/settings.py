2e8a6193e4fb241ea5b221dbb97613deba04b6a5
Prakash <prakashthapa617@gmail.com>
2025-12-11 09:04:43 +0545

    ..

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -236,6 +236,8 @@ else:
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'noreply.zestideologycoop@gmail.com'
+# EMAIL_HOST_PASSWORD = 'sntnyasfqvelzzjl'
 EMAIL_HOST_USER = 'prakashthapa617@gmail.com'
 EMAIL_HOST_PASSWORD = 'sntnyasfqvelzzjl'
 
